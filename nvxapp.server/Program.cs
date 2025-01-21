f525d43061d3187db7eab6af7b47eba1b5ac02ff
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-01-21 17:33:31 +0100

    Aggiunto swagger

--- nvxapp.server/Program.cs
+++ nvxapp.server/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,30 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+// Aggiungi Swagger
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    // Configura l'uso di Swagger
+    app.UseSwagger();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
+    // Avvia automaticamente la pagina Swagger nel browser predefinito
+    var urlSw = "https://localhost:7146/swagger/index.html";
+    Process.Start(new ProcessStartInfo(urlSw) { UseShellExecute = true });
+
 }
 
 app.UseHttpsRedirection();
@@ -23,5 +43,8 @@ app.UseDefaultFiles(); // Serve automaticamente il file "index.html"
 app.UseStaticFiles();
 
 app.MapControllers();
+
+
+
 
 app.Run();
