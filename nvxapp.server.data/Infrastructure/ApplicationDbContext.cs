623dea848a4162cf27e677ec2d5b8b42476d86e1
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-02-17 09:25:48 +0100

    Aggiunto ApplicationDbContext_Infrastructure

--- nvxapp.server.data/Infrastructure/ApplicationDbContext.cs
+++ nvxapp.server.data/Infrastructure/ApplicationDbContext.cs
@@ -16,19 +16,10 @@ namespace nvxapp.server.data.Infrastructure
     
 
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    public partial class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         private readonly IConfiguration _configuration;
         private readonly IHttpContextAccessor? _httpContextAccessor;
-
-        public DbSet<MyTable> MyTables { get; set; }
-        public DbSet<Dealer> Dealer { get; set; }
-        public DbSet<UserDealer> UserDealer { get; set; }
-        public DbSet<Company> Company { get; set; }
-        public DbSet<UserCompany> UserCompany { get; set; }
-
-
-
 
         private static string _baseSchema = "public";
         public string? _currentSchema = "public";
@@ -72,136 +63,7 @@ namespace nvxapp.server.data.Infrastructure
         }
 
 
-        private void Gen_InitDB(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ApplicationUser>(entity => { entity.ToTable("AspNetUsers", _baseSchema); });
-            modelBuilder.Entity<ApplicationRole>(entity => { entity.ToTable("AspNetRoles", _baseSchema); });
-
-            modelBuilder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("AspNetUserRoles", _baseSchema); });
-            modelBuilder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("AspNetUserClaims", _baseSchema); });
-            modelBuilder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("AspNetUserLogins", _baseSchema); });
-            modelBuilder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("AspNetRoleClaims", _baseSchema); });
-            modelBuilder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("AspNetUserTokens", _baseSchema); });
-        }
-        private void Gen_MyTable(ModelBuilder modelBuilder)
-        {
-            // Configura altre entità nello schema appropriato
-            modelBuilder.Entity<MyTable>(entity =>
-            {
-                entity.ToTable("MyTable", _currentSchema);
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Descrizione).IsRequired();
-
-                // Crea un indice univoco 
-                entity.HasIndex(e => new { e.Descrizione }).IsUnique();
-            });
-        }
-        private void Gen_DealerAndCompany_Init(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Dealer>(entity =>
-            {
-                entity.ToTable("Dealer", _baseSchema);
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Descrizione).IsRequired();
-                // Crea un indice univoco 
-                entity.HasIndex(e => new { e.Descrizione }).IsUnique();
-            });
-
-            modelBuilder.Entity<UserDealer>(entity =>
-            {
-                entity.ToTable("UserDealer", _baseSchema);
-                entity.HasKey(e => e.Id);
-
-                // Crea un indice univoco su due colonne
-                entity.HasIndex(e => new { e.IdDealer, e.IdAspNetUsers }).IsUnique();
-            });
-
-            modelBuilder.Entity<Company>(entity =>
-            {
-                entity.ToTable("Company", _baseSchema);
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Descrizione).IsRequired();
-                // Crea un indice univoco 
-                entity.HasIndex(e => new { e.Descrizione }).IsUnique();
-            });
-
-            modelBuilder.Entity<UserCompany>(entity =>
-            {
-                entity.ToTable("UserCompany", _baseSchema);
-                entity.HasKey(e => e.Id);
-
-                // Crea un indice univoco su due colonne
-                entity.HasIndex(e => new { e.IdCompany, e.IdAspNetUsers }).IsUnique();
-            });
-
-            #region CONFIGURE RELATIONSHIP ONE-TO-MANY 
-
-
-            #region Dealer-Company
-            modelBuilder.Entity<Company>()
-                        .HasOne(md => md.DealerNavigation)
-                        .WithMany(d => d.Company)
-                        .HasForeignKey(md => md.IdDealer);
-
-            modelBuilder.Entity<Dealer>()
-                        .HasMany(o => o.Company)
-                        .WithOne(i => i.DealerNavigation)
-                        .OnDelete(DeleteBehavior.Cascade); //MODIFICARE SENNO RASA VIA TUTTO
-
-
-            #endregion
-
-            #region Dealer-UserDealer
-            modelBuilder.Entity<UserDealer>()
-                        .HasOne(md => md.DealerNavigation)
-                        .WithMany(d => d.UserDealer)
-                        .HasForeignKey(md => md.IdDealer);
-
-            modelBuilder.Entity<Dealer>()
-                        .HasMany(o => o.UserDealer)
-                        .WithOne(i => i.DealerNavigation)
-                        .OnDelete(DeleteBehavior.Cascade);
-
-
-            modelBuilder.Entity<UserDealer>()
-                        .HasOne(md => md.AspNetUsersNavigation)
-                        .WithMany(d => d.UserDealer)
-                        .HasForeignKey(md => md.IdAspNetUsers);
-
-            modelBuilder.Entity<ApplicationUser>()
-                        .HasMany(o => o.UserDealer)
-                        .WithOne(i => i.AspNetUsersNavigation)
-                        .OnDelete(DeleteBehavior.Cascade);
-            #endregion
-
-            #region Company-UserCompany
-
-            modelBuilder.Entity<UserCompany>()
-                        .HasOne(md => md.CompanyNavigation)
-                        .WithMany(d => d.UserCompany)
-                        .HasForeignKey(md => md.IdCompany);
-
-            modelBuilder.Entity<Company>()
-                        .HasMany(o => o.UserCompany)
-                        .WithOne(i => i.CompanyNavigation)
-                        .OnDelete(DeleteBehavior.Cascade);
-
-
-            modelBuilder.Entity<UserCompany>()
-                        .HasOne(md => md.AspNetUsersNavigation)
-                        .WithMany(d => d.UserCompany)
-                        .HasForeignKey(md => md.IdAspNetUsers);
-
-            modelBuilder.Entity<ApplicationUser>()
-                        .HasMany(o => o.UserCompany)
-                        .WithOne(i => i.AspNetUsersNavigation)
-                        .OnDelete(DeleteBehavior.Cascade);
-
-            #endregion
-
-            #endregion
-
-        }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             SharedSchema.CurrentSchema = getSchema;
@@ -210,11 +72,9 @@ namespace nvxapp.server.data.Infrastructure
 
             modelBuilder.HasDefaultSchema(_baseSchema);
 
-            Gen_InitDB(modelBuilder);
+            Define_Table_DbContext_Infrastructure(modelBuilder);
 
-            Gen_MyTable(modelBuilder);
 
-            Gen_DealerAndCompany_Init(modelBuilder);
 
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ nvxapp.server.data/Infrastructure/ApplicationDbContext_Infrastructure.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using nvxapp.server.data.Entities.Public;
+using nvxapp.server.data.Entities.Tenant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nvxapp.server.data.Infrastructure
+{
+    public partial class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public DbSet<MyTable> MyTables { get; set; }
+        public DbSet<Dealer> Dealer { get; set; }
+        public DbSet<UserDealer> UserDealer { get; set; }
+        public DbSet<Company> Company { get; set; }
+        public DbSet<UserCompany> UserCompany { get; set; }
+
+
+        private void Define_Table_DbContext_Infrastructure(ModelBuilder modelBuilder)
+        {
+            Gen_InitDB(modelBuilder);
+            Gen_MyTable(modelBuilder);
+            Gen_DealerAndCompany_Init(modelBuilder);
+        }
+
+        private void Gen_InitDB(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>(entity => { entity.ToTable("AspNetUsers", _baseSchema); });
+            modelBuilder.Entity<ApplicationRole>(entity => { entity.ToTable("AspNetRoles", _baseSchema); });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("AspNetUserRoles", _baseSchema); });
+            modelBuilder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("AspNetUserClaims", _baseSchema); });
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("AspNetUserLogins", _baseSchema); });
+            modelBuilder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("AspNetRoleClaims", _baseSchema); });
+            modelBuilder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("AspNetUserTokens", _baseSchema); });
+        }
+        private void Gen_MyTable(ModelBuilder modelBuilder)
+        {
+            // Configura altre entità nello schema appropriato
+            modelBuilder.Entity<MyTable>(entity =>
+            {
+                entity.ToTable("MyTable", _currentSchema);
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Descrizione).IsRequired();
+
+                // Crea un indice univoco 
+                entity.HasIndex(e => new { e.Descrizione }).IsUnique();
+            });
+        }
+        private void Gen_DealerAndCompany_Init(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dealer>(entity =>
+            {
+                entity.ToTable("Dealer", _baseSchema);
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Descrizione).IsRequired();
+                // Crea un indice univoco 
+                entity.HasIndex(e => new { e.Descrizione }).IsUnique();
+            });
+
+            modelBuilder.Entity<UserDealer>(entity =>
+            {
+                entity.ToTable("UserDealer", _baseSchema);
+                entity.HasKey(e => e.Id);
+
+                // Crea un indice univoco su due colonne
+                entity.HasIndex(e => new { e.IdDealer, e.IdAspNetUsers }).IsUnique();
+            });
+
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.ToTable("Company", _baseSchema);
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Descrizione).IsRequired();
+                // Crea un indice univoco 
+                entity.HasIndex(e => new { e.Descrizione }).IsUnique();
+            });
+
+            modelBuilder.Entity<UserCompany>(entity =>
+            {
+                entity.ToTable("UserCompany", _baseSchema);
+                entity.HasKey(e => e.Id);
+
+                // Crea un indice univoco su due colonne
+                entity.HasIndex(e => new { e.IdCompany, e.IdAspNetUsers }).IsUnique();
+            });
+
+            #region CONFIGURE RELATIONSHIP ONE-TO-MANY 
+
+
+            #region Dealer-Company
+            modelBuilder.Entity<Company>()
+                        .HasOne(md => md.DealerNavigation)
+                        .WithMany(d => d.Company)
+                        .HasForeignKey(md => md.IdDealer);
+
+            modelBuilder.Entity<Dealer>()
+                        .HasMany(o => o.Company)
+                        .WithOne(i => i.DealerNavigation)
+                        .OnDelete(DeleteBehavior.Cascade); //MODIFICARE SENNO RASA VIA TUTTO
+
+
+            #endregion
+
+            #region Dealer-UserDealer
+            modelBuilder.Entity<UserDealer>()
+                        .HasOne(md => md.DealerNavigation)
+                        .WithMany(d => d.UserDealer)
+                        .HasForeignKey(md => md.IdDealer);
+
+            modelBuilder.Entity<Dealer>()
+                        .HasMany(o => o.UserDealer)
+                        .WithOne(i => i.DealerNavigation)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<UserDealer>()
+                        .HasOne(md => md.AspNetUsersNavigation)
+                        .WithMany(d => d.UserDealer)
+                        .HasForeignKey(md => md.IdAspNetUsers);
+
+            modelBuilder.Entity<ApplicationUser>()
+                        .HasMany(o => o.UserDealer)
+                        .WithOne(i => i.AspNetUsersNavigation)
+                        .OnDelete(DeleteBehavior.Cascade);
+            #endregion
+
+            #region Company-UserCompany
+
+            modelBuilder.Entity<UserCompany>()
+                        .HasOne(md => md.CompanyNavigation)
+                        .WithMany(d => d.UserCompany)
+                        .HasForeignKey(md => md.IdCompany);
+
+            modelBuilder.Entity<Company>()
+                        .HasMany(o => o.UserCompany)
+                        .WithOne(i => i.CompanyNavigation)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<UserCompany>()
+                        .HasOne(md => md.AspNetUsersNavigation)
+                        .WithMany(d => d.UserCompany)
+                        .HasForeignKey(md => md.IdAspNetUsers);
+
+            modelBuilder.Entity<ApplicationUser>()
+                        .HasMany(o => o.UserCompany)
+                        .WithOne(i => i.AspNetUsersNavigation)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            #endregion
+
+            #endregion
+
+        }
+
+
+      
+
+    }
+}
