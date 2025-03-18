cdda1fc511ea04120f2de64111e2c30448a744ff
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-03-18 09:55:37 +0100

    Aggiunto entity ExpenseReport

---
+++ nvxapp.server.data/Entities/Tenant/AttendanceTracking/ExpenseReport.cs
@@ -0,0 +1,17 @@
+﻿using nvxapp.server.data.Entities.Public;
+using System.ComponentModel.DataAnnotations;
+
+namespace nvxapp.server.data.Entities.Tenant
+{
+    public class ExpenseReport : BaseEntity
+    {
+        [Required]
+        public required string IdAspNetUsers { get; set; }
+        public ApplicationUser? AspNetUsersNavigation { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+
+
+
+}
