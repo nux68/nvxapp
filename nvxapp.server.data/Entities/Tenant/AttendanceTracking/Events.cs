b28dfb2155b32073a557022cc6156285a772342d
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-03-18 09:46:24 +0100

    Aggiunta entity Events

---
+++ nvxapp.server.data/Entities/Tenant/AttendanceTracking/Events.cs
@@ -0,0 +1,41 @@
+﻿using nvxapp.server.data.Entities.Public;
+using System.ComponentModel.DataAnnotations;
+
+namespace nvxapp.server.data.Entities.Tenant
+{
+    public class Events : BaseEntity
+    {
+
+        [Required]
+        public required string IdAspNetUsers { get; set; }
+        public ApplicationUser? AspNetUsersNavigation { get; set; }
+
+        public DateTime Date { get; set; }
+        public EventType Type { get; set; }
+        public EventState State { get; set; }
+
+        // Campo per oggetto JSON
+        public string? DataJson { get; set; }
+
+
+    }
+
+
+    public enum EventType
+    {
+        ClockRequest = 1,
+        JustRequest = 2,
+        ExpenseReportRequest = 3
+    }
+
+    public enum EventState
+    {
+        Entered = 1,
+        Deleted = 1,
+        Rejected,
+        Approved,
+
+    }
+
+
+}
