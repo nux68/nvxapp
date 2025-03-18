5f78ffefd3f03984f73ed7171ffb2bc4dbcdbfa2
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-03-18 16:59:17 +0100

    Piccole var

---
+++ nvxapp.server.data/Entities/Tenant/AttendanceTracking/EmploymentRelationship.cs
@@ -0,0 +1,20 @@
+﻿using nvxapp.server.data.Entities.Public;
+using System.ComponentModel.DataAnnotations;
+
+namespace nvxapp.server.data.Entities.Tenant
+{
+
+    /*
+     dati anagrafici
+     */
+    public class EmploymentRelationship : BaseEntity
+    {
+        [Required]
+        public required string IdAspNetUsers { get; set; }
+        public ApplicationUser? AspNetUsersNavigation { get; set; }
+
+        public DateTime? HiringDate { get; set; } // assunzione
+        public DateTime? DismissalDate { get; set; } //lic
+    }
+
+}
---
+++ nvxapp.server.data/Entities/Tenant/AttendanceTracking/UserPersonalData.cs
@@ -0,0 +1,23 @@
+﻿using nvxapp.server.data.Entities.Public;
+using System.ComponentModel.DataAnnotations;
+
+namespace nvxapp.server.data.Entities.Tenant
+{
+
+    /*
+     dati anagrafici
+     */
+    public class UserPersonalData : BaseEntity
+    {
+        [Required]
+        public required string IdAspNetUsers { get; set; }
+        public ApplicationUser? AspNetUsersNavigation { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+    }
+
+
+
+}
