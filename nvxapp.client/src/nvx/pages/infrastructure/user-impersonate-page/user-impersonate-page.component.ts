86ba6ca27849b5dd8ee977f7888ee0c54fc5b4cb
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-04-15 17:34:38 +0200

    Modificato form Goto

--- nvxapp.client/src/nvx/pages/infrastructure/user-impersonate-page/user-impersonate-page.component.ts
+++ nvxapp.client/src/nvx/pages/infrastructure/user-impersonate-page/user-impersonate-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserCronologyModel, UserNavigationService } from '../../../Utility/user-navigation.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
+import { ButtonItem, UserInterfaceService } from '../../../Utility/user-interface.service';
 
 @Component({
   selector: 'app-user-impersonate-page',
@@ -16,11 +17,27 @@ export class UserImpersonatePageComponent  implements OnInit {
 
   public userCronology: UserCronologyModel[] | null = null;
 
+  public buttonbarGoTo:  ButtonItem;
+  public buttonbarLogOut: ButtonItem;
+  public buttonbar: ButtonItem[] = [];
+
   constructor(public userNavigationService: UserNavigationService,
               private navCtrl: NavController,
+              protected userInterfaceService: UserInterfaceService,
               private fb: FormBuilder,) {
 
     this.title = 'UserImpersonate';
+
+    this.buttonbarGoTo = userInterfaceService.Btn_Impersona;
+    this.buttonbarGoTo.event = this._handleUserGoBackClick;
+    this.buttonbar.push(this.buttonbarGoTo);
+
+    this.buttonbarLogOut = userInterfaceService.Btn_LogOut;
+    this.buttonbarLogOut.event = this._handleLogOutClick;
+    this.buttonbar.push(this.buttonbarLogOut);
+
+
+
   }
 
   ionViewWillEnter() {
@@ -39,6 +56,15 @@ export class UserImpersonatePageComponent  implements OnInit {
     this.navCtrl.navigateForward('/home');
   }
 
-  
+  private _handleLogOutClick = (param: object) => {
+
+    this.LogOut()
+
+  }
+  private _handleUserGoBackClick = (param: object) => {
+
+    this.UserGoBack()
+
+  }
 
 }
