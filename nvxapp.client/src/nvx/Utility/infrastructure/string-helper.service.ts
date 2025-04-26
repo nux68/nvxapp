cc3afa29b17daa0369524c134553c2c0feb34563
NUX68 <nello68@hotmail.com>
2025-04-26 21:16:00 +0200

    aggiunto toJSONString

--- nvxapp.client/src/nvx/Utility/infrastructure/string-helper.service.ts
+++ nvxapp.client/src/nvx/Utility/infrastructure/string-helper.service.ts
@@ -11,4 +11,10 @@ export class StringHelperService {
       return str.replace(/[^a-zA-Z0-9]/g, '');
   }
 
+  toJSONString<T>(obj: T): string {
+    return JSON.stringify(obj, (key, value) => {
+      // Se il valore è una data, convertila in stringa ISO
+      return value instanceof Date ? value.toISOString() : value;
+    });
+  }
 }
