36ae988229acae0e76ce10121a211e83cbaf101d
NUX68 <nello68@hotmail.com>
2025-04-27 15:32:59 +0200

    Aggiunte func per le date e orari

--- nvxapp.client/src/nvx/Utility/infrastructure/string-helper.service.ts
+++ nvxapp.client/src/nvx/Utility/infrastructure/string-helper.service.ts
@@ -32,5 +32,41 @@ export class StringHelperService {
   }
   //const timbratura = GenericHelper.fromJSONString(jsonString, Dip_GG_TimbraturaModel);
 
+  DateTimeCurr_To_ISOString(): string {
+    const now = new Date();
+    const localOffset = now.getTimezoneOffset() * 60000; // Offset in millisecondi
+    const localTime = new Date(now.getTime() - localOffset).toISOString().slice(0, 16);
 
+    return localTime;
+  }
+
+  DateTime_To_ddmmyyyy_hhmm(date: Date): string {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+
+  }
+
+  DateTime_To_ddmmyyyy(date: Date): string {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    //const hours = date.getHours().toString().padStart(2, '0');
+    //const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${day}/${month}/${year}`;
+
+  }
+
+  DateTime_To_hhmm(date: Date): string {
+    //const day = date.getDate().toString().padStart(2, '0');
+    //const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    //const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+
+  }
 }
