cb44c36eeaefdb833a0e639bb8d1bc8d848b9e08
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-05-05 15:54:27 +0200

    Aggiunti valori consetiti a tabella Just

--- nvxapp.server.data/Entities/Tenant/GestionePresenze/Par_Giustificativi.cs
+++ nvxapp.server.data/Entities/Tenant/GestionePresenze/Par_Giustificativi.cs
@@ -26,11 +26,17 @@ namespace nvxapp.server.data.Entities.Tenant
         [MaxLength(7)]
         public string? TextColor { get; set; }
 
+        //public JustInputValoriConsentiti InputValoriConsentiti { get; set; }
 
         public ICollection<Dip_GG_Giustificativi>? Dip_GG_Giustificativi { get; set; }
 
     }
 
-
+    public enum JustInputValoriConsentiti
+    {
+        SoloInteraGiornate,
+        DaA,
+        Tutti
+    }
 
 }
