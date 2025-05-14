977413303dd33a0dd08a0d2a2f4736f59e61ea5b
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-05-14 10:27:52 +0200

    Piccole var

--- nvxapp.client/src/nvx/pages/GestionePresenze/time-sheet-admin-page/time-sheet-admin-page.component.ts
+++ nvxapp.client/src/nvx/pages/GestionePresenze/time-sheet-admin-page/time-sheet-admin-page.component.ts
@@ -23,4 +23,38 @@ export class TimeSheetAdminPageComponent implements OnInit {
 
   ngOnInit() { }
 
+
+  onPeriodChange(period: { year: number, month: number } | undefined): void {
+    //this.selectedSedeId = sediId;
+    console.log('Parent: Period changed to:', period.year + period.month);
+    //this.fetchRelevantData();
+  }
+
+  onSedeChanged(sediId: number | undefined): void {
+    //this.selectedSedeId = sediId;
+    //console.log('Parent: Sede ID changed to:', sediId);
+    //this.fetchRelevantData();
+  }
+  onRepartiChanged(repartoIds: number[] | undefined): void {
+    //this.selectedRepartoIds = repartoIds;
+    //console.log('Parent: Reparto IDs changed to:', repartoIds);
+    // Note: User list will be re-evaluated by the navigation component,
+    // leading to onCurrentUserChanged potentially being called.
+    // We might not need to call fetchRelevantData() here if onCurrentUserChanged also calls it.
+    // However, if we want to show data aggregated by reparti even if no user is selected,
+    // then we might fetch here. For this example, let's assume we act on user change.
+  }
+  onCurrentUserChanged(userId: string | undefined): void {
+    //this.currentNavigationUserId = userId;
+    //console.log('Parent: Current User ID changed to:', userId);
+    //this.fetchRelevantData();
+  }
+
+  onAllUsersInSelectionChanged(userIds: string[] | undefined): void {
+    //console.log('Parent: All available User IDs in current selection:', userIds);
+
+  }
+
+
+
 }
