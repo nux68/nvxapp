3119a703d65e8e1387301db9520c95b11da24ef1
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-05-20 12:01:44 +0200

    Aggiunto Dip_GG_Causali con AI

---
+++ nvxapp.server.service/Mappers/Tenant/GestionePresenze/Dip_GG_Causali_AND_Dip_GG_CausaliModel_Mapper.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using nvxapp.server.data.Entities.Tenant;
+using nvxapp.server.service.ClientServer_Service.GestionePresenze.Dip_GG_CausaliService.Models;
+
+namespace nvxapp.server.service.Mappers.Tenant.GestionePresenze
+{
+    public class Dip_GG_Causali_To_Dip_GG_CausaliModel_Mapper : Profile
+    {
+        public Dip_GG_Causali_To_Dip_GG_CausaliModel_Mapper()
+        {
+            CreateMap<Dip_GG_Causali, Dip_GG_CausaliModel>();
+        }
+    }
+
+    public class Dip_GG_CausaliModel_To_Dip_GG_Causali_Mapper : Profile
+    {
+        public Dip_GG_CausaliModel_To_Dip_GG_Causali_Mapper()
+        {
+            CreateMap<Dip_GG_CausaliModel, Dip_GG_Causali>();
+        }
+    }
+}
