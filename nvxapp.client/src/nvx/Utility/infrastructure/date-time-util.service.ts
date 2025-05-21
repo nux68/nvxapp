ccdedef256b843822c2c9043186994def1c5b0ba
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-05-21 17:19:38 +0200

    Aggiunta DateTo_ggmmyyyy_hhmm

--- nvxapp.client/src/nvx/Utility/infrastructure/date-time-util.service.ts
+++ nvxapp.client/src/nvx/Utility/infrastructure/date-time-util.service.ts
@@ -14,12 +14,22 @@ export class DateTimeUtilService {
     return hours * 60 + minutes;
   }
 
-  public DateToSDate(date: Date): string {
+  public DateTo_ggmmyyyy(date: Date): string {
     const d = new Date(date);
     const day = String(d.getDate()).padStart(2, '0');
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const year = d.getFullYear();
     return `${day}/${month}/${year}`;
+  }
+
+  public DateTo_ggmmyyyy_hhmm(date: Date): string {
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    const hours = String(d.getHours()).padStart(2, '0');
+    const minutes = String(d.getMinutes()).padStart(2, '0');
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
   }
 
 }
