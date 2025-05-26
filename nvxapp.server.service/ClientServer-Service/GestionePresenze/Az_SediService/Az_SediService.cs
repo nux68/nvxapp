04aeccd900b525eafabdb2cb130b4969901184d4
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-05-26 14:24:29 +0200

    Fix lista sedi

--- nvxapp.server.service/ClientServer-Service/GestionePresenze/Az_SediService/Az_SediService.cs
+++ nvxapp.server.service/ClientServer-Service/GestionePresenze/Az_SediService/Az_SediService.cs
@@ -1,18 +1,19 @@
-﻿using nvxapp.server.service.ClientServer_Service.ModelsBase;
-using nvxapp.server.Base;
-using nvxapp.server.service.Interfaces;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using nvxapp.server.service.ServerModels;
+using nvxapp.server.Base;
 using nvxapp.server.data.Entities.Public;
 using nvxapp.server.data.Repositories.Public;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using nvxapp.server.data.Repositories.Tenant.GestionePresenze;
-
-using nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediService.Models;
+using nvxapp.server.service.ClientServer_Service.GestionePresenze._utility;
+using nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_CfgService.Models;
 using nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediRepartoService.Models;
+using nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediService.Models;
+using nvxapp.server.service.ClientServer_Service.ModelsBase;
+using nvxapp.server.service.Interfaces;
+using nvxapp.server.service.ServerModels;
 
 namespace nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediService
 {
@@ -20,6 +21,7 @@ namespace nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediSer
     public class Az_SediService : ServiceBase, IAz_SediService
     {
         private readonly IAz_SediRepository _az_SediRepository;
+        private readonly IGestionePresenzeUserUtility _gestionePresenzeUserUtility;
 
         public Az_SediService(IMapper mapper,
                                   UserManager<ApplicationUser> userManager,
@@ -27,10 +29,12 @@ namespace nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediSer
                                   IOptions<JwtParameter> jwtParameter,
                                   IHttpContextAccessor httpContextAccessor,
                                   IConfiguration configuration,
+                                  IGestionePresenzeUserUtility gestionePresenzeUserUtility,
 
                                   IAz_SediRepository az_SediRepository) : base(mapper , userManager  , aspNetUsersRepository, jwtParameter, configuration, httpContextAccessor)
         {
             _az_SediRepository = az_SediRepository;
+            _gestionePresenzeUserUtility = gestionePresenzeUserUtility;
         }
 
         public virtual async Task<GenericResult<Az_Sedi_GetAll_OutModel>> GetAll(GenericRequest<Az_Sedi_GetAll_InModel> model, Boolean isSubProcess)
@@ -40,9 +44,18 @@ namespace nvxapp.server.service.ClientServer_Service.GestionePresenze.Az_SediSer
                 Az_Sedi_GetAll_OutModel retVal = new Az_Sedi_GetAll_OutModel();
 
 
-                var az_Sedi = _az_SediRepository.FindAll(x => x.Id > 0).ToList();
+                int IdCompany;
+                int.TryParse(this.CurrentCompany, out IdCompany);
 
-                retVal.Az_Sedi = _mapper.Map<List<Az_SediModel>>(az_Sedi);
+                Company_DATA_COMB_AzAna_AzSedi_AzReparto_Az_Cfg company_DATA_COMB_AzAna_AzSedi_AzReparto = await _gestionePresenzeUserUtility.Get_AzAna_AzSedi_AzReparto_Az_Cfg(IdCompany, true);
+                if (company_DATA_COMB_AzAna_AzSedi_AzReparto != null && company_DATA_COMB_AzAna_AzSedi_AzReparto.az_Cfg != null)
+                {
+                    var az_Sedi = _az_SediRepository.FindAll(x => x.IdAz_Anagrafica == company_DATA_COMB_AzAna_AzSedi_AzReparto!.az_Anagrafica!.Id).ToList();
+                    retVal.Az_Sedi = _mapper.Map<List<Az_SediModel>>(az_Sedi);    
+                }    
+
+
+                
 
                 //eliminare
                 // Nessun 'await' qui
