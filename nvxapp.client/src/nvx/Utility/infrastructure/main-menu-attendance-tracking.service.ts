268cc0326ebd4e0ea46adda9f8a60dd37f26e978
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2025-05-30 14:36:08 +0200

    Aggiunta etichetta Parametri al menu azienda

--- nvxapp.client/src/nvx/Utility/infrastructure/main-menu-attendance-tracking.service.ts
+++ nvxapp.client/src/nvx/Utility/infrastructure/main-menu-attendance-tracking.service.ts
@@ -16,19 +16,22 @@ export class MainMenuAttendanceTrackingService implements iMainMenuService {
   public get Pages4CompanyAdmin(): MainMenuItem[] {
 
     return [
-      { menuType: MenuType.MenuItem, zorder: 0,   title: 'Richieste Admin', roles: ['CompanyPowerAdmin', 'CompanyAdmin'], component: "JustificationListPageComponent", url: '/requestlistadmin', icon: 'ellipse' },
-      { menuType: MenuType.MenuItem, zorder: 100, title: 'Calendario HR'  , roles: ['CompanyPowerAdmin']                , component: "TimeSheetPowerAdminPageComponent", url: '/poweradmintimesheet', icon: 'ellipse' },
-      { menuType: MenuType.MenuItem, zorder: 200, title: 'Giustificativi' , roles: ['CompanyPowerAdmin']                , component: "JustificationListPageComponent", url: '/justificationlist', icon: 'ellipse' },
-      { menuType: MenuType.MenuItem, zorder: 300, title: 'Reparti'        , roles: ['CompanyPowerAdmin']                , component: "DepartmentListPageComponent", url: '/departmentlist', icon: 'ellipse' },
-      { menuType: MenuType.MenuItem, zorder: 400, title: 'Configurazione' , roles: ['CompanyPowerAdmin']                , component: "CompanyCfgPageComponent", url: '/companycfgedit', icon: 'ellipse' },
-      { menuType: MenuType.MenuItem, zorder: 500, title: 'Calendario'     , roles: ['CompanyAdmin']                     , component: "TimeSheetAdminPageComponent", url: '/admintimesheet', icon: 'ellipse' },
-      { menuType: MenuType.MenuItem, zorder: 600, title: 'Utenti reparto' , roles: ['CompanyAdmin']                     , component: "UserDepartmentListPageComponent", url: '/userdepartmentlist', icon: 'people' },
+      { menuType: MenuType.MenuItem, zorder: 1000,   title: 'Richieste Admin'  , roles: ['CompanyPowerAdmin', 'CompanyAdmin'], component: "JustificationListPageComponent", url: '/requestlistadmin', icon: 'ellipse' },
+      { menuType: MenuType.MenuItem, zorder: 2000,   title: 'Calendario HR'    , roles: ['CompanyPowerAdmin']                , component: "TimeSheetPowerAdminPageComponent", url: '/poweradmintimesheet', icon: 'ellipse' },
+      { menuType: MenuType.MenuItem, zorder: 2000,   title: 'Calendario'       , roles: ['CompanyAdmin']                     , component: "TimeSheetAdminPageComponent", url: '/admintimesheet', icon: 'ellipse' },
+      { menuType: MenuType.MenuItem, zorder: 3000,   title: 'Utenti reparto'   , roles: ['CompanyAdmin']                     , component: "UserDepartmentListPageComponent", url: '/userdepartmentlist', icon: 'people' },
+
+      { menuType: MenuType.MenuNote, zorder: 100000, title: 'Parametri'                                                      , component: '', url: '', icon: '' },
+      { menuType: MenuType.MenuItem, zorder: 100100, title: 'Giustificativi' , roles: ['CompanyPowerAdmin']                  , component: "JustificationListPageComponent", url: '/justificationlist', icon: 'ellipse' },
+      { menuType: MenuType.MenuItem, zorder: 100200, title: 'Reparti'        , roles: ['CompanyPowerAdmin']                  , component: "DepartmentListPageComponent", url: '/departmentlist', icon: 'ellipse' },
+      { menuType: MenuType.MenuItem, zorder: 100300, title: 'Configurazione' , roles: ['CompanyPowerAdmin']                  , component: "CompanyCfgPageComponent", url: '/companycfgedit', icon: 'ellipse' },
+      
     ];
 
   }
   public get Pages4User(): MainMenuItem[] {
     return [
-      { menuType: MenuType.MenuItem, zorder: 0, title: 'Richieste', component: "RequestListUserPageComponent", url: '/requestlistuser', icon: 'person' },
+      { menuType: MenuType.MenuItem, zorder: 0,   title: 'Richieste', component: "RequestListUserPageComponent", url: '/requestlistuser', icon: 'person' },
       { menuType: MenuType.MenuItem, zorder: 100, title: 'Calendario', component: "TimeSheetUserPageComponent", url: '/usertimesheet', icon: 'person' },
       { menuType: MenuType.MenuItem, zorder: 200, title: 'Tmbratura', component: "TimeClockUserPageComponent", url: '/timeclockuser', icon: 'person' },
       { menuType: MenuType.MenuItem, zorder: 300, title: 'Richiesta giustificativo', component: "RequestJustificationUserPageComponent", url: '/requestjustificationuser', icon: 'person' },
