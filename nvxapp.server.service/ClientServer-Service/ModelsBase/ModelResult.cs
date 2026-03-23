f4c2869f987ce68ac9c9184346e8bdfa49707dcf
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2026-03-23 10:40:09 +0100

    Aggiunto IsHashChanged

--- nvxapp.server.service/ClientServer-Service/ModelsBase/ModelResult.cs
+++ nvxapp.server.service/ClientServer-Service/ModelsBase/ModelResult.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq.Expressions;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace nvxapp.server.service.ClientServer_Service.ModelsBase
@@ -49,32 +48,5 @@ namespace nvxapp.server.service.ClientServer_Service.ModelsBase
         List<Message> Messages { get; set; }
     }
 
-
-
-    public class HashModel
-    {
-        public string Hash { get; set; }
-
-        public string CalcolaHash<T>(T instance, params Expression<Func<T, object>>[] campi)
-        {
-            var values = new List<string>();
-
-            foreach (var campo in campi)
-            {
-                var compiled = campo.Compile();
-                var value = compiled(instance);
-
-                values.Add(value?.ToString() ?? "null");
-            }
-
-            var raw = string.Join("|", values);
-
-            using var sha = SHA256.Create();
-            var bytes = Encoding.UTF8.GetBytes(raw);
-            var hashBytes = sha.ComputeHash(bytes);
-
-            return Convert.ToHexString(hashBytes);
-        }
-    }
 
 }
