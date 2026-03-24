1d778b17e6714bff6531066b3d314757f59eec62
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2026-03-24 16:37:00 +0100

    Aggiunto enum Sign

---
+++ nvxapp.server.data/Extensions/Varie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nvxapp.server.data.Extensions
+{
+   
+    public enum SignNeutral { 
+        Down = -1, 
+        Up = 1 }
+
+    public enum SignWithNeutral { 
+        Down = -1, 
+        Neutral = 0,
+        Up = 1 }
+
+}
