dee12b71c5752fbdbeefe9796c37b39533dc9cc3
Antonio Pugliese <antonio.pugliese@pslpaghe.it>
2026-04-23 16:04:45 +0200

    Aggiurnato RequestClockingUserPageComponent

--- nvxapp.client/src/nvx/pages/GestionePresenze/request-clocking-user-page/request-clocking-user-page.component.ts
+++ nvxapp.client/src/nvx/pages/GestionePresenze/request-clocking-user-page/request-clocking-user-page.component.ts
@@ -7,6 +7,12 @@ import { Dip_GG_Richiesta_Body_Timbratura, Dip_GG_Richiesta_Send_InModel, StatoR
 import { GenericRequest } from '../../../ClientServer-Service/ModelsBase/generic-request';
 import { ButtonItem, UserInterfaceService } from '../../../Utility/infrastructure/user-interface.service';
 import { RefresherService } from '../../../Utility/GestionePresenze/refresher.service';
+import { Dip_GG_Timbratura_StampPrepare_InModel, Dip_GG_Timbratura_StampPrepare_OutModel } from '../../../ClientServer-Service/GestionePresenze/Dip_GG_Timbratura/Models/dip-gg-timbratura-model';
+import { BasePageConfirmCancelComponent } from '../../_BASE/base-page-confirm-cancel/base-page-confirm-cancel.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DipGGTimbraturaService } from '../../../ClientServer-Service/GestionePresenze/Dip_GG_Timbratura/dip-gg-timbratura.service';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Par_AttivitaModel } from '../../../ClientServer-Service/GestionePresenze/Par_Attivita/Models/par-attivita-model';
 
 @Component({
   selector: 'app-request-clocking-user-page',
@@ -14,76 +20,70 @@ import { RefresherService } from '../../../Utility/GestionePresenze/refresher.se
   styleUrls: ['./request-clocking-user-page.component.scss'],
   standalone: false
 })
-export class RequestClockingUserPageComponent implements OnInit {
+export class RequestClockingUserPageComponent extends BasePageConfirmCancelComponent<Dip_GG_Timbratura_StampPrepare_OutModel> {
   public title: string;
 
-  //////
-  public buttonbar: ButtonItem[] = [];
-  public btnAnnulla: ButtonItem;
-  public btnInvia: ButtonItem;
-  //////
+  
+  
   public dateTime: string;
   public formattedDate: string;
   public formattedTime: string;
-  public supervisors: string[];
+  
   public notes: string;
+  public par_AttivitaList: Par_AttivitaModel[] = []
 
-  constructor(private navCtrl: NavController,
-              private refresherService: RefresherService,
-              public userNavigationService: UserNavigationService,
-              private userInterfaceService: UserInterfaceService,
-              private dipGGRichiestaService: DipGGRichiestaService,
-              private stringHelperService: StringHelperService) {
-
-    this.title = 'Richiedi timbratura';
-
-    this.btnInvia = userInterfaceService.Btn_Invia;
-    this.btnInvia.event = this._handleButtonConfirmClick;
-    this.buttonbar.push(this.btnInvia);
-    this.btnAnnulla = userInterfaceService.Btn_Annulla;
-    this.btnAnnulla.event = this._handleButtonCancelClick;
-    this.buttonbar.push(this.btnAnnulla);
- 
+  constructor(
+    protected override navCtrl: NavController,
+    protected override userInterfaceService: UserInterfaceService,
+    protected override fb: FormBuilder,
+    private dipGGTimbraturaService: DipGGTimbraturaService,
+    private stringHelperService: StringHelperService,
+    private dipGGRichiestaService: DipGGRichiestaService,
+    private refresherService: RefresherService
+  ) {
+    super(navCtrl, userInterfaceService, fb);
   }
 
-  ngOnInit() { }
+  get Title(): string { return 'Richiedi timbratura'; }
 
-  ionViewWillEnter() {
-
-    // Initialize with current date and time
-    const now = new Date();
-    // Format date for ion-datetime (ISO format)
-    //this.dateTime = now.toISOString();
-
-    this.dateTime = this.stringHelperService.DateCurr_To_ISOString();
-
-
-    this.formattedDate = this.stringHelperService.Date_To_S_ddmmyyyy(now)
-    this.formattedTime = this.stringHelperService.Date_To_S_hhmm(now);
-
-    this.supervisors = ['manzo.admin'];
-    this.notes = '';
+  get EditForm(): FormGroup {
+    return this.fb.group({
+      currentDate: [null, [Validators.required]],
+      idPar_Attivita: [null, [Validators.required]],
+    });
   }
-   
+
+  override ionViewWillEnter() {
+      const now = new Date();
+      this.formattedDate = this.stringHelperService.Date_To_S_ddmmyyyy(now)
+      this.formattedTime = this.stringHelperService.Date_To_S_hhmm(now);
+
+    super.ionViewWillEnter();
+  }
+
   updateDateTime(event: any) {
     const selectedDate = new Date(event.detail.value);
     this.formattedDate = this.stringHelperService.Date_To_S_ddmmyyyy(selectedDate)
     this.formattedTime = this.stringHelperService.Date_To_S_hhmm(selectedDate);
   }
 
-  addSupervisor() {
-    // In a real app, this would open a modal or dropdown to select from available supervisors
-    // For demo purposes, we'll just add a mock supervisor
-    if (!this.supervisors.includes('new.supervisor')) {
-      this.supervisors.push('new.supervisor');
-    }
-  }
+  LoadData = (): Observable<Dip_GG_Timbratura_StampPrepare_OutModel | null> => {
+    const request = new GenericRequest<Dip_GG_Timbratura_StampPrepare_InModel>(Dip_GG_Timbratura_StampPrepare_InModel);
+    return this.dipGGTimbraturaService.PrepareStamp(request).pipe(
+      map(res => {
 
-  removeSupervisor(index: number) {
-    this.supervisors.splice(index, 1);
-  }
+        this.par_AttivitaList = res.data.par_Attivita;
 
-  submitRequest() {
+        return res.data;
+      }),
+      catchError(error => {
+        console.error('Errore durante PrepareStamp:', error);
+        return of(null);
+      })
+    );
+  };
+
+  SaveData = (editModel: Dip_GG_Timbratura_StampPrepare_OutModel): Observable<boolean> => {
 
     let request_rich = new GenericRequest<Dip_GG_Richiesta_Send_InModel>(Dip_GG_Richiesta_Send_InModel);
 
@@ -96,7 +96,6 @@ export class RequestClockingUserPageComponent implements OnInit {
 
     }
 
-
     let dip_GG_Richiesta_Body_Timbratura: Dip_GG_Richiesta_Body_Timbratura = new Dip_GG_Richiesta_Body_Timbratura();
     dip_GG_Richiesta_Body_Timbratura.hhmm = this.formattedTime;
 
@@ -108,24 +107,28 @@ export class RequestClockingUserPageComponent implements OnInit {
     request_rich.data.dip_GG_Richiesta.dataA = this.formattedDate;
     request_rich.data.dip_GG_Richiesta.dati = this.stringHelperService.toJSONString(dip_GG_Richiesta_Body_Timbratura);
 
-    this.dipGGRichiestaService.Send(request_rich).subscribe(res => {
+    return this.dipGGRichiestaService.Send(request_rich).pipe(map(() => {
       this.refresherService.Dip_GG_Richiesta_triggerRefresh();
-      this.navCtrl.back();
-    });
+      return true;
+    } ));
+    
 
+
+  };
+
+  onStartDatetimeChange(event: any) {
+    const value = event?.detail?.value;
+    if (value) {
+      const selected = new Date(value);
+      
+      this.formattedDate = this.formatDate(selected);
+      
+    }
   }
 
-  private _handleButtonConfirmClick = (param: object) => {
-
-    this.submitRequest();
-
+  formatDate(date: Date): string {
+    return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
   }
 
-  private _handleButtonCancelClick = (param: object) => {
-
-    //this.navCtrl.navigateForward('/home');
-    this.navCtrl.back();
-
-  }
 
 }
